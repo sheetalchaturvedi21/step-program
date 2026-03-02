a1ad13fd18a9f348bbdb13bc29fabef9ffd0b0b7
Sheetal Chaturvedi <yourgithubemail@gmail.com>
2026-03-02 13:33:30 +0530

    UC6: Refactor banner using static helper methods for O, P, S

---
+++ OOPSBannerApp6.java
@@ -0,0 +1,60 @@
+public class OOPSBannerApp6 {
+
+    public static void main(String[] args) {
+
+        String gap = "   ";
+
+        String[] o = buildO();
+        String[] p = buildP();
+        String[] s = buildS();
+
+        String[] banner = new String[7];
+
+        for (int i = 0; i < 7; i++) {
+            banner[i] = String.join(gap, o[i], o[i], p[i], s[i]);
+        }
+
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+
+    // Helper method for letter O
+    public static String[] buildO() {
+        return new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        };
+    }
+
+    // Helper method for letter P
+    public static String[] buildP() {
+        return new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                " ***** ",
+                "*      ",
+                "*      ",
+                "*      "
+        };
+    }
+
+    // Helper method for letter S
+    public static String[] buildS() {
+        return new String[]{
+                " ***** ",
+                "*      ",
+                "*      ",
+                " ***** ",
+                "      *",
+                "      *",
+                " ***** "
+        };
+    }
+}
