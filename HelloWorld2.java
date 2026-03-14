6f580001f896a699723dcc60895c916f368d2760
Sheetal Chaturvedi <yourgithubemail@gmail.com>
2026-03-14 13:36:30 +0530

    UC2: Display Hello with command line argument

---
+++ HelloWorld2.java
@@ -0,0 +1,11 @@
+public class HelloWorld2 {
+    public static void main(String[] args) {
+
+        if(args.length > 0) {
+            System.out.println("Hello " + args[0]);
+        } else {
+            System.out.println("Please provide a name");
+        }
+
+    }
+}
