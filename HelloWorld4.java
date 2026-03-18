947926a1cc06c7906deffb5811233a1fb714c233
Sheetal Chaturvedi <yourgithubemail@gmail.com>
2026-03-18 14:04:07 +0530

    UC4: Display Hello with multiple command line arguments

---
+++ HelloWorld4.java
@@ -0,0 +1,13 @@
+public class HelloWorld4 {
+    public static void main(String[] args) {
+
+        if(args.length > 0) {
+            for(int i = 0; i < args.length; i++) {
+                System.out.println("Hello " + args[i]);
+            }
+        } else {
+            System.out.println("Hello World");
+        }
+
+    }
+}
